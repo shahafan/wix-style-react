e2763a298e199df848679fdd57ef7ad93e0b2d0d
argshook <argshook@gmail.com>
2017-10-17 15:16:44 +0300

    chore(button): refactor to not use buttonLayout
    `<Button/>` should take care of itself, less imports, no prop spread, no
    omits, just simple component
    
    also auto generated documentation

--- src/Backoffice/Button/Button.js
+++ src/Backoffice/Button/Button.js
@@ -1,67 +1,107 @@
 import React from 'react';
-import {any, func, node, string} from 'prop-types';
-import styles from './Button.scss';
+import {any, func, node, string, bool, oneOf} from 'prop-types';
+import classnames from 'classnames';
+
 import WixComponent from '../../BaseComponents/WixComponent';
-import ButtonLayout from '../../ButtonLayout/ButtonLayout';
-import omit from 'omit';
+
+import styles from './Button.scss';
+
+const affixIcon = (className, icon, height) => {
+  const iconSize = height === 'small' ? '8px' : height === 'medium' ? '12px' : '16px';
+  const dataHook = className === styles.prefix ? 'btn-prefix' : 'btn-suffix';
+
+  return (
+    icon ?
+      <div className={className} data-hook={dataHook}>
+        {React.cloneElement(icon, {size: iconSize})}
+      </div> :
+      null
+  );
+};
 
 class Button extends WixComponent {
+  static displayName = 'Button'
+
   static propTypes = {
-    ...ButtonLayout.propTypes,
     children: any,
     id: string,
+
+    /** The icon to show at the begging of the button */
     prefixIcon: node,
+
+    /** The icon to show at the end of the button */
     suffixIcon: node,
     type: string,
     onClick: func,
     onMouseEnter: func,
-    onMouseLeave: func
+    onMouseLeave: func,
+    active: bool,
+    disabled: bool,
+
+    /** The size of the button, can be small, medium, large or x-large */
+    height: oneOf(['small', 'medium', 'large']),
+    hover: bool,
+
+    /** The theme of the button */
+    theme: oneOf([
+      'transparent', 'fullred', 'fullgreen', 'fullpurple', 'emptyred',
+      'emptygreen', 'emptybluesecondary', 'emptyblue', 'emptypurple', 'fullblue',
+      'login', 'emptylogin', 'transparentblue', 'whiteblue', 'whiteblueprimary',
+      'whitebluesecondary', 'close-standard', 'close-dark', 'close-transparent', 'icon-greybackground',
+      'icon-standard', 'icon-standardsecondary', 'icon-white', 'icon-whitesecondary'
+    ])
   }
 
-  static defaultProps = ButtonLayout.defaultProps;
-
-  constructor(props) {
-    super(props);
-    this.addPrefix = this.addPrefix.bind(this);
-    this.addSuffix = this.addSuffix.bind(this);
-    this.addIcon = this.addIcon.bind(this);
-  }
-
-  addIcon(className, icon, height) {
-    const iconSize = height === 'small' ? '8px' : height === 'medium' ? '12px' : '16px';
-    const dataHook = className === styles.prefix ? 'btn-prefix' : 'btn-suffix';
-    return (
-      icon ?
-        <div className={className} data-hook={dataHook}>
-          {React.cloneElement(icon, {size: iconSize})}
-        </div> :
-        null
-    );
-  }
-
-  addPrefix() {
-    return this.addIcon(styles.prefix, this.props.prefixIcon, this.props.height);
-  }
-
-  addSuffix() {
-    return this.addIcon(styles.suffix, this.props.suffixIcon, this.props.height);
+  static defaultProps = {
+    height: 'medium',
+    theme: 'fullblue',
+    type: 'button'
   }
 
   render() {
-    const {disabled, onClick, children, type, onMouseEnter, onMouseLeave} = this.props;
-    const buttonLayoutProps = omit(['id', 'onClick', 'prefixIcon', 'suffixIcon', 'type'], this.props);
+    const {
+      active,
+      children,
+      disabled,
+      height,
+      hover,
+      onClick,
+      onMouseEnter,
+      onMouseLeave,
+      prefixIcon,
+      suffixIcon,
+      theme,
+      type
+    } = this.props;
+
     return (
-      <ButtonLayout {...buttonLayoutProps}>
-        <button onClick={onClick} disabled={disabled} type={type} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-          {this.addPrefix()}
+      <button
+        {...{
+          className: classnames(
+            styles.button,
+            styles[theme],
+            {
+              [styles.hover]: hover,
+              [styles.active]: active,
+              [styles.disabled]: disabled,
+              [styles[`height${height}`]]: height !== 'medium'
+            }
+          ),
+          onClick,
+          disabled,
+          type,
+          onMouseEnter,
+          onMouseLeave
+        }}
+        >
+        <div className={styles.inner}>
+          {affixIcon(styles.prefix, prefixIcon, height)}
           {children}
-          {this.addSuffix()}
-        </button>
-      </ButtonLayout>
+          {affixIcon(styles.suffix, suffixIcon, height)}
+        </div>
+      </button>
     );
   }
 }
-
-Button.displayName = 'Button';
 
 export default Button;
--- stories/Backoffice/Button/index.js
+++ stories/Backoffice/Button/index.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import {storiesOf} from '@storybook/react';
-import Markdown from '../../utils/Components/Markdown';
-import TabbedView from '../../utils/Components/TabbedView';
 import CodeExample from '../../utils/Components/CodeExample';
-import Readme from '../../../src/Backoffice/Button/README.md';
-import ReadmeTestKit from '../../../src/Backoffice/Button/README.TESTKIT.md';
+import readmeTestkit from '../../../src/Backoffice/Button/README.TESTKIT.md';
 
 import ExampleControlled from './ExampleControlled';
 import ExampleControlledRaw from '!raw-loader!./ExampleControlled';
 import IconsExample from './ExampleWithIcons';
 import IconsExampleRaw from '!raw-loader!./ExampleWithIcons';
 
-storiesOf('Backoffice', module)
-  .add('Button', () => (
-    <TabbedView tabs={['API', 'TestKits']}>
-      <div>
-        <Markdown source={Readme}/>
-        <h1>Example</h1>
-        <CodeExample title="With icons" code={IconsExampleRaw}>
-          <IconsExample/>
-        </CodeExample>
-        <CodeExample title="Controlled" code={ExampleControlledRaw}>
-          <ExampleControlled/>
-        </CodeExample>
-      </div>
-      <Markdown source={ReadmeTestKit}/>
-    </TabbedView>
-  ));
+import story from '../../utils/Components/Story';
+
+import component from 'wix-style-react/Button';
+import source from '!raw-loader!wix-style-react/Backoffice/Button/Button';
+
+story({
+  category: 'Backoffice',
+  name: 'Button',
+  readmeTestkit,
+  source,
+  component,
+  componentProps: {
+    children: 'Hello world!'
+  },
+  examples: (
+    <div>
+      <CodeExample title="With icons" code={IconsExampleRaw}>
+        <IconsExample/>
+      </CodeExample>
+
+      <CodeExample title="Controlled" code={ExampleControlledRaw}>
+        <ExampleControlled/>
+      </CodeExample>
+    </div>
+  )
+});
