458ba73e3e3b415d58cbc8ce8618b4ae20e6798a
argshook <argshook@gmail.com>
2017-10-17 16:04:52 +0300

    chore(ButtonLayout): remove duplicate from Backoffice/ButtonLayout

--- src/Backoffice/ButtonLayout/index.js
+++ src/Backoffice/ButtonLayout/index.js
@@ -1,1 +1,9 @@
-export {default} from './ButtonLayout.js';
+/**
+  * This file is here only to not break whoever may still be using
+  * Backoffice/ButtonLayout
+  *
+  * we currenlty have this same component at src/ButtonLayout so no real
+  * reason to duplicate it here
+  */
+
+export {default} from '../../ButtonLayout';
